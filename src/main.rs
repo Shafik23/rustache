1dc53b818810c6ca9170dfa21b01a13d1bb8cddc
Shafik Amin <shafik23@gmail.com>
2021-03-27 17:20:12 -0700

    Added POST functionality

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,40 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
+    let http_get = b"GET / HTTP/1.1\r\n";
     let request = String::from_utf8_lossy(&buffer[..]).to_string();
+    println!("Request Length: {}", request.len());
 
-    stream.write(get_response(request).as_bytes()).unwrap();
+    if buffer.starts_with(http_get) {}
+
+    let response = match buffer.starts_with(http_get) {
+        true => make_get_response(request),
+        _ => make_post_response(request),
+    };
+
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
-fn get_response(request: String) -> String {
-    // println!("Request: {}", request);
+fn make_post_response(request: String) -> String {
+    let content = request;
+    let response = format!(
+        "HTTP/1.1 201 OK-POST\r\nContent-Length: {}\r\n\r\n{}",
+        content.len(),
+        content
+    );
+
+    println!("POST Response: {}", response);
+    response
+}
+
+fn make_get_response(request: String) -> String {
+    println!("Request: {}", request);
 
     let content = fs::read_to_string("response.html").unwrap();
 
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "HTTP/1.1 200 OK-GET\r\nContent-Length: {}\r\n\r\n{}",
         content.len(),
         content
     );
